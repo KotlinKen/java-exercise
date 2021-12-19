8b196c26c940f9faec83b98a14b3a32a0d5f8fad
kotlin <kotlinken@gmail.com>
2021-12-19 23:20:37 +0900

    main sout test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(1);
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(1);
+
+
     }
 }
